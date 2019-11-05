1cccab169e48b26fa398a7c0dd007a7b05b8d806
Kapa <tylahkapa@gmail.com>
2019-11-05 17:31:16 +1000

    Created A First Person Player Controller
    First Person player controller supports walking and looking around. Can also invert the Y axis, just needs to be put into a menu somewhere down the line. Thanks for the help, Brackeys!

---
+++ GO2019-Game/Assets/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 12f;
+    public float gravity = -9.81f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    
+    CharacterController controller;
+    Vector3 velocity;
+    bool isGrounded;
+    float moveX = 0f;
+    float moveZ = 0f;
+
+    private void Start() {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update() {
+        moveX = Input.GetAxis("Horizontal");
+        moveZ = Input.GetAxis("Vertical");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        
+        Vector3 move = transform.right * moveX + transform.forward * moveZ;
+        controller.Move(move * speed * Time.deltaTime);
+
+        if(isGrounded && velocity.y < 0){
+            velocity.y = -2f;
+        } else  if (!isGrounded){
+            velocity.y += gravity * Time.deltaTime;
+            controller.Move(velocity * Time.deltaTime);
+        }
+    }
+}
---
+++ GO2019-Game/Assets/Scripts/Player/MouseLook.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    public bool invertedY = false;
+
+    public Transform playerBody;
+    float xRotation = 0f;
+    float mouseX = 0f;
+    float mouseY = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        if(!invertedY){
+            xRotation -= mouseY;
+        } else{
+            xRotation += mouseY;
+        }
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);        
+    }
+}
