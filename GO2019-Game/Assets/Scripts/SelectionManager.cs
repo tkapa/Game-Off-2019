37baa9234fa113a9bf19ba156ab4c4a9e07d76c5
Kapa <tylahkapa@gmail.com>
2019-11-06 12:54:48 +1000

    Interactable Object Highlighting
    When a gameobject is on the selectable layer, the selection manager on the player object will highlight the object to let the player know that they can interact with it.

---
+++ GO2019-Game/Assets/Scripts/SelectionManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionManager : MonoBehaviour
+{
+    [SerializeField] private LayerMask pickupMask;
+    [SerializeField] private float pickupDistance = 5f;
+    [SerializeField] private Material highlightMaterial;
+    [SerializeField] private string selectableTag;
+
+    private Material defaultMaterial;
+    private Transform currentSelection;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentSelection != null){
+            ChangeMaterial(currentSelection.GetComponent<Renderer>(), defaultMaterial);
+            currentSelection = null;
+        }
+
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit, pickupDistance, pickupMask)){
+            var selection = hit.transform;
+            var selectionRenderer = selection.GetComponent<Renderer>();
+            if(selectionRenderer != null){
+                ChangeMaterial(selection.GetComponent<Renderer>(), highlightMaterial);
+            }
+            currentSelection = selection;    
+        }
+    }
+
+    void ChangeMaterial(Renderer renderer, Material material){
+        defaultMaterial = renderer.material;
+        renderer.material = material;
+    }
+}
