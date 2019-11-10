15425b90622d8e4866e00aba7212449fcea81355
Levi Stenton <levistenton@gmail.com>
2019-11-10 19:45:38 +1000

    Added Player Health and Screenshake

---
+++ GO2019-Game/Assets/Scripts/Enemy/EnemyAttack.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+
+    PlayerUIManager playerManager;
+    public float damage;
+    public float damageTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerManager = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerUIManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            Debug.Log("Hit");
+            playerManager.TakeDamage(damage);
+            StartCoroutine(ResetCollider());
+        }
+    }
+
+    IEnumerator ResetCollider()
+    {
+        gameObject.GetComponent<CapsuleCollider>().enabled = false;
+        yield return new WaitForSeconds(damageTimer);
+        gameObject.GetComponent<CapsuleCollider>().enabled = true;
+    }
+}
---
+++ GO2019-Game/Assets/Scripts/Player/PlayerUIManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerUIManager : MonoBehaviour
+{
+
+    [Header("Health Bar")]
+    public Slider playerHealthBar;
+    public float playerHealth;
+    public float playerHealthMax;
+    public float playerHealthMin;
+
+    public GameObject playerCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerHealthBar.maxValue = playerHealthMax;
+        playerHealthBar.minValue = playerHealthMin;
+        playerHealth = playerHealthMax;
+        playerHealthBar.value = playerHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        
+    }
+
+    public void TakeHealing(float healing)
+    {
+        playerHealth += healing;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        playerHealth -= damage;
+        playerHealthBar.value = playerHealth;
+        StartCoroutine(ScreenShakeing(0.1f, 0.05f));
+        if(playerHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    IEnumerator ScreenShakeing(float duration, float magnitude)
+    {
+    Vector3 originalCamPos = Camera.main.transform.localPosition;
+
+    float elapsed = 0.0f;    
+    
+    while (elapsed < duration) 
+    { 
+        float x = Random.Range(-1f, 1f) * magnitude;
+        float y = Random.Range(-1f, 1f) * magnitude;
+        
+        Camera.main.transform.localPosition = new Vector3(x, y, originalCamPos.z);
+
+        elapsed += Time.deltaTime;
+            
+        yield return null;
+    }
+    
+    Camera.main.transform.localPosition = originalCamPos;
+    
+    }
+}
