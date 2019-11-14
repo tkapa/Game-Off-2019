96555958d33ddcd21ac76a9935615876f1088a54
Kapa <tylahkapa@gmail.com>
2019-11-14 12:36:48 +1000

    Enemy Movement and Spawning
    Enemy now literally just walks towards the player and spawns in the level when it is finished generating.

---
+++ GO2019-Game/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float moveSpeed = 3f;
+    
+    private GameObject player;
+    private CharacterController controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        player = FindObjectOfType<PlayerMovement>().gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(player.transform.position);
+
+        Vector3 move = player.transform.position - transform.position;
+        controller.Move(move * moveSpeed * Time.deltaTime);
+    }
+}
---
+++ GO2019-Game/Assets/Scripts/Enemy/EnemySpawning.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawning : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+
+    private LevelGeneration levelGeneration;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelGeneration = FindObjectOfType<LevelGeneration>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(levelGeneration.stoppedGeneration){
+            Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+        }
+    }
+}
