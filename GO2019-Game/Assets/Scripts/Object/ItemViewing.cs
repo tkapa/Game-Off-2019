7732c5134dbbb766beb6c12793dc090bbde57891
Levi Stenton <levistenton@gmail.com>
2019-11-17 01:03:14 +1000

    Added in potions and changed the pickuppable items to sprites

---
+++ GO2019-Game/Assets/Scripts/Object/ItemViewing.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemViewing : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(GameObject.FindGameObjectWithTag("Player").transform);
+    }
+}
