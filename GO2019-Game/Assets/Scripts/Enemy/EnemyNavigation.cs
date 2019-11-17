107093a2ab00a5cc1ccf86f4ee069abcf4cc6ef2
Kapa <tylahkapa@gmail.com>
2019-11-17 17:16:37 +1000

    Enemy Navigation now uses Navmesh. Also put in boundaries for the generated level.

---
+++ GO2019-Game/Assets/Scripts/Enemy/EnemyNavigation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyNavigation : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent agent;
+
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<PlayerMovement>().gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(player.transform.position);
+    }
+}
