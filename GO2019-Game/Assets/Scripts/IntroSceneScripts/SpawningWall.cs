1cf2747f003a8dac6e2e8d47fe978f9c9069418f
Kapa <tylahkapa@gmail.com>
2019-11-28 13:06:55 +1000

    no message

--- GO2019-Game/Assets/Scripts/IntroSceneScripts/SpawningWall.cs
+++ GO2019-Game/Assets/Scripts/IntroSceneScripts/SpawningWall.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 public class SpawningWall : MonoBehaviour
 {
     public GameObject wall = null;
+    public WallShiftingSound shiftingSound = null;
+
+    private void Start() {
+        if(GetComponentInChildren<WallShiftingSound>()){
+            shiftingSound = GetComponentInChildren<WallShiftingSound>();
+        }
+    }
 
     private void OnTriggerExit(Collider other) {
         //Put wall moving sound effect here.
+        shiftingSound.PlaySound();
         wall.SetActive(true);
         Destroy(this);
     }
---
+++ GO2019-Game/Assets/Scripts/Object/WallShiftingSound.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallShiftingSound : MonoBehaviour
+{
+    public AudioSource source;
+
+    public void PlaySound(){
+        source.pitch = Random.Range(-1.5f, 1.5f);
+        source.PlayOneShot(source.clip);
+    }
+}
